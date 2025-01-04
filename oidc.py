ee0a0ab8d642ba49bbfefd9c4e776b2a52f46964
Lukas Steiner <lukas.steiner@steinheilig.de>
2025-01-04 17:02:22 +0100

    feat: add key generation and rotation

--- oidc.py
+++ oidc.py
@@ -1,36 +1,103 @@
+import os
 import jwt
 import base64
+import uuid
+import json
+import schedule
+
 from datetime import datetime, timedelta, timezone
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa, ec
+from threading import Lock
 
 
 class OIDCProvider:
+    SUPPORTED_SIGNING_ALGORITHMS = ["RS256", 'RS384', 'RS512',
+                                    'ES256', 'ES384', 'ES512']
+    SIGNING_ALGORITHM_CURVE = {ec.SECP256R1: "P-256",
+                               ec.SECP384R1: "P-384",
+                               ec.SECP521R1: "P-521", }
+
     def __init__(self, app, conf):
         self.conf = conf
-        self.private_key = self.load_private_key()
-        self.public_key = self.load_public_key()
+        self.mutex = Lock()
+        self.jwks_keys = list()
+        self.load_jwks_state()
+        self.generate_private_key()
         self.app = app
         self.setup_routes()
+        self.scheduler = schedule.Scheduler()
+        self.scheduler.every(self.conf.key_rotation_period).hours.do(
+            self.generate_private_key)
 
-    def load_private_key(self):
-        """Load private key from file"""
-        with open(self.conf.private_key_path, 'rb') as key_file:
-            private_key = serialization.load_pem_private_key(
-                key_file.read(),
-                password=None
-            )
-        return private_key
+    def load_jwks_state(self):
+        """Load JWKS keys cfrom state file"""
+        self.mutex.acquire()
+        if os.path.exists(self.conf.jwks_state):
+            with open(self.conf.jwks_state, 'rb') as state:
+                self.jwks_keys = json.load(state)
+        self.mutex.release()
 
-    def load_public_key(self):
-        """Load public key from file"""
-        with open(self.conf.public_key_path, 'rb') as key_file:
-            public_key = serialization.load_pem_public_key(
-                key_file.read()
-            )
-        return public_key
+    def generate_private_key(self):
+        """Generate private key for signing tokens"""
+        algo = self.conf.signing_algorithm
+        if algo == 'RS256':
+            private_key = rsa.generate_private_key(
+                public_exponent=65537, key_size=2048)
+        elif algo == 'RS384':
+            private_key = rsa.generate_private_key(
+                public_exponent=65537, key_size=3072)
+        elif algo == 'RS512':
+            private_key = rsa.generate_private_key(
+                public_exponent=65537, key_size=4096)
+        elif algo == 'ES256':
+            private_key = ec.generate_private_key(ec.SECP256R1())
+        elif algo == 'ES384':
+            private_key = ec.generate_private_key(ec.SECP384R1())
+        elif algo == 'ES512':
+            private_key = ec.generate_private_key(ec.SECP521R1())
+        else:
+            raise ValueError(f"Unsupported key type, supported are {
+                             self.SUPPORTED_SIGNING_ALGORITHMS}")
+
+        public_numbers = private_key.public_key().public_numbers()
+        if isinstance(private_key, rsa.RSAPrivateKey):
+            jwks_key = {
+                'kty': 'RSA',
+                'use': 'sig',
+                'kid': uuid.uuid4(),
+                'n': self.int_to_base64(public_numbers.n),
+                'e': self.int_to_base64(public_numbers.e),
+                'alg': self.conf.signing_algorithm,
+            }
+        elif isinstance(private_key, ec.EllipticCurvePrivateKey):
+            jwks_key = {
+                'kty': 'EC',
+                'crv': self.SIGNING_ALGORITHM_CURVE.get(type(private_key)),
+                'use': 'sig',
+                'kid': str(uuid.uuid4()),
+                'x': self.int_to_base64(public_numbers.x),
+                'y': self.int_to_base64(public_numbers.y),
+                'alg': self.conf.signing_algorithm,
+            }
+        else:
+            raise TypeError("Unsupported private key type")
+
+        self.mutex.acquire()
+
+        self.private_key = private_key
+        self.jwks_keys.append(jwks_key)
+
+        # remove old public keys from JWKS
+        if len(self.jwks_keys) > 3:
+            self.jwks_keys.pop(0)
+
+        # persist jwks state
+        with open(self.conf.jwks_state, 'w') as state:
+            json.dump(self.jwks_keys, state)
+
+        self.mutex.release()
 
     def setup_routes(self):
-        # TODO: This route is not tested yet and only a dummy for implementing a openid discovery endpoint
         @self.app.route('/.well-known/openid-configuration')
         def openid_configuration():
             return {
@@ -38,26 +105,12 @@ class OIDCProvider:
                 'jwks_uri': f"{self.conf.issuer_url}/.well-known/jwks.json",
                 'response_types_supported': ['id_token'],
                 'subject_types_supported': ['public'],
-                'id_token_signing_alg_values_supported': ['RS256'],
-                'scopes_supported': ['openid', 'profile', 'email'],
-                'claims_supported': ['sub', 'iss', 'aud', 'exp', 'iat']
+                'id_token_signing_alg_values_supported': self.SUPPORTED_SIGNING_ALGORITHMS,
             }
 
         @self.app.route('/.well-known/jwks.json')
         def jwks():
-            public_numbers = self.public_key.public_numbers()
-            return {
-                'keys': [
-                    {
-                        'kty': 'RSA',
-                        'use': 'sig',
-                        'kid': '1',
-                        'n': self.int_to_base64(public_numbers.n),
-                        'e': self.int_to_base64(public_numbers.e),
-                        'alg': 'RS256'
-                    }
-                ]
-            }
+            return {'keys': self.jwks_keys}
 
     def int_to_base64(self, value):
         """Convert an integer to a Base64URL-encoded string"""
@@ -70,6 +123,7 @@ class OIDCProvider:
 
     def create_token(self, sub, metadata):
         """Generate a new token with given subject and metadata attributes"""
+        self.scheduler.run_pending()
         now = datetime.now(timezone.utc)
         expires_at = now + timedelta(hours=self.conf.token_lifetime)
         payload = {
@@ -80,4 +134,8 @@ class OIDCProvider:
             'exp': expires_at,
         }
 
-        return jwt.encode(payload | metadata, self.private_key, algorithm='RS256'), expires_at
+        self.mutex.acquire()
+        token = jwt.encode(payload | metadata, self.private_key,
+                           algorithm=self.conf.signing_algorithm)
+        self.mutex.release()
+        return token, expires_at
